5d079b349de0b419da7e80509485a30192ac1dd6
marcelosost <marcelo.shutze@gmail.com>
2023-04-14 10:54:43 -0300

    modifiquei o brete

--- Javinha/src/Exercicio2.java
+++ Javinha/src/Exercicio2.java
@@ -2,17 +2,16 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercicio2 {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-
-        System.out.println("Insira o valor A");
-        double valora = sc.nextDouble();
-        System.out.println("Insira o valor B");
-		double valorb = sc.nextDouble();
-        double soma = valora + valorb;
-		System.out.printf("SOMA = %.0f%n", + soma);
 		
+		double R, A, pi = 3.14159;
+ 	    R = sc.nextDouble();
+	    A = pi * R * R;
+	    System.out.printf("A=%.4f%n", A);
+
 		sc.close();
-		}
+	}
 }
