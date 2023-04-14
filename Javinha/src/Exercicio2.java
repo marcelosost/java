0982bfcaf357f2e7060d42582b92b54c7955af32
marcelosost <marcelo.shutze@gmail.com>
2023-04-14 15:33:20 -0300

    ajustemo!

--- Javinha/src/Exercicio2.java
+++ Javinha/src/Exercicio2.java
@@ -1,12 +1,15 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+import javax.print.event.PrintJobListener;
+
 public class Exercicio2 {
   public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+    System.out.println("Digite aqui o valor a ser calculado:");
 		double R, A, pi = 3.14159;
  	    R = sc.nextDouble();
 	    A = pi * R * R;
