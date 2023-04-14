8d442e1d0342f92f3897d470dbcd4aa2399bd4e9
marcelosost <marcelo.shutze@gmail.com>
2023-04-14 10:37:09 -0300

    teste de commit pelo git

--- Javinha/src/App.java
+++ Javinha/src/App.java
@@ -1,5 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insira o valor A");
+        double valora = sc.nextDouble();
+        System.out.println("Insira o valor B");
+		double valorb = sc.nextDouble();
+        double soma = valora + valorb;
+		System.out.printf("SOMA = %.0f%n", + soma);
+		
+		sc.close();
+		}
 }
